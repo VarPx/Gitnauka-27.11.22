3a4dbbc4473343c5645e005b04bda81951e5e6d8
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-11-28 14:17:29 +0100

    Zmiana w drugim repo

--- src/main/java/pl/burchardt/Main.java
+++ src/main/java/pl/burchardt/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    private void displayText() {
+        System.out.println("zmiana w drugim repo");
+    }
 }
