3724cc5644013f9f19c29224fa32b0c67bf28a11
Jakub Burchardt <73552568+VarPx@users.noreply.github.com>
2022-11-28 14:24:02 +0100

    Hello Dagmara i Jakub

--- src/main/java/pl/burchardt/Main.java
+++ src/main/java/pl/burchardt/Main.java
@@ -3,6 +3,7 @@ package pl.burchardt;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Jakub!");
+        System.out.println("Hello Dagmara!");
     }
     private void displayText() {
         System.out.println("zmiana w drugim repo");
